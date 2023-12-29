d39293be4ac34acae620ae514cfd2ad7ab87a33d
Md Nurnobi Hosen <47949519+nurnobishanto@users.noreply.github.com>
2023-12-29 15:04:39 +0600

    update command print

--- routes/command.php
+++ routes/command.php
@@ -11,31 +11,37 @@ Route::prefix('command')->group(function (){
     Route::get('/clear-cache', function (){
         App::setLocale(session('locale'));
         Artisan::call('cache:clear');
+        echo "<pre>";
         print_r(Artisan::output()) ;
     });
     Route::get('/clear-config', function (){
         App::setLocale(session('locale'));
         Artisan::call('config:clear');
+        echo "<pre>";
         print_r(Artisan::output()) ;
     });
     Route::get('/clear-route', function (){
         App::setLocale(session('locale'));
         Artisan::call('route:clear');
+        echo "<pre>";
         print_r(Artisan::output()) ;
     });
     Route::get('/optimize', function (){
         App::setLocale(session('locale'));
         Artisan::call('optimize');
+        echo "<pre>";
         print_r(Artisan::output()) ;
     });
     Route::get('/clear-optimize', function (){
         App::setLocale(session('locale'));
         Artisan::call('optimize:clear');
+        echo "<pre>";
         print_r(Artisan::output()) ;
     });
     Route::get('/migrate', function (){
         App::setLocale(session('locale'));
         Artisan::call('migrate');
+        echo "<pre>";
         print_r(Artisan::output()) ;
     });
 });
