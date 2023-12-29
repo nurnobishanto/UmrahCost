6eed9910e3c5835f579fe36af6f8ca441e2be05b
Md Nurnobi Hosen <47949519+nurnobishanto@users.noreply.github.com>
2023-12-29 15:10:48 +0600

    upcd-5

--- routes/command.php
+++ routes/command.php
@@ -11,22 +11,23 @@ Route::prefix('command')->group(function (){
     Route::get('/clear-cache', function (){
         App::setLocale(session('locale'));
         Artisan::call('cache:clear');
-        return __('notification.cache_cleared');
+        return Artisan::output();
     });
     Route::get('/clear-config', function (){
         App::setLocale(session('locale'));
         Artisan::call('config:clear');
-        return  __('notification.config_cleared');
+        
+        return Artisan::output();
     });
     Route::get('/clear-route', function (){
         App::setLocale(session('locale'));
         Artisan::call('route:clear');
-        return __('notification.route_cleared');
+        return Artisan::output();
     });
     Route::get('/optimize', function (){
         App::setLocale(session('locale'));
         Artisan::call('optimize');
-        return __('notification.optimized');
+        return Artisan::output();
     });
     Route::get('/clear-optimize', function (){
         App::setLocale(session('locale'));
