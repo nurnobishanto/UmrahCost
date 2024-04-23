2dee46c5c184e9a1e448b73d7e7f268b9bc0e189
Md Nurnobi Hosen <47949519+nurnobishanto@users.noreply.github.com>
2024-04-23 15:56:26 +0600

    Update admin.php

--- routes/admin.php
+++ routes/admin.php
@@ -28,6 +28,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'admin/', 'as' => 'admin.', 'middleware' => ['auth', 'webAdmin']], function () {
 
+    Route::get('/', [DashboardController::class, 'index'])->name('admin');
     Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
     Route::get('change/password', [ProfileController::class, 'changePassword'])->name('changePassword');
     Route::post('change/password/update', [ProfileController::class, 'changePasswordUpdate'])->name('changePassword.update');
